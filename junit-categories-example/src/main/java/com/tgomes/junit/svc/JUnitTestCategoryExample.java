f4d879b3e194eb34f656e90be0aa2b1a5d50e7e8
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-15 15:30:47 -0300

    write first test for the first category

---
+++ junit-categories-example/src/main/java/com/tgomes/junit/svc/JUnitTestCategoryExample.java
@@ -0,0 +1,23 @@
+package com.tgomes.junit.svc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import com.tgomes.junit.cat.intf.FunctionalGroupTests1;
+
+public class JUnitTestCategoryExample {
+
+	@Test
+	@Category(FunctionalGroupTests1.class)
+	public void testFunctionalTests1Test1() {
+		Integer numberInLoop = 0;
+		for (int i = 0; i < 1000; i++) {
+			numberInLoop++;
+		}
+		System.out.println("FunctionalGroupTests1: testFunctionalTests1Test1");
+		assertTrue(numberInLoop.getClass() == Integer.class);
+	}
+
+}
